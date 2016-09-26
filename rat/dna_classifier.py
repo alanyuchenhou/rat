4c6208d7ff8afc333d973369320ff6e4041ace52
Yuchen Hou <houych@gmail.com>
2016-09-26 16:57:32 -0700

    use balanced training set and full testing set

--- rat/dna_classifier.py
+++ rat/dna_classifier.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 from tensorflow.contrib import learn
 from rat.clean import get_segments, get_indices
 
-SEGMENT_LENGTH = 1000
+SEGMENT_LENGTH = 500
 
 
 def char_cnn_model(x, y):
@@ -31,21 +31,29 @@ def main():
     segments = get_segments('rat-chr20.fa', indices, SEGMENT_LENGTH)
     x = segments['sequence']
     y = segments['label']
+    positive_samples = segments[segments['label'] == 1]
+    negative_samples = segments[segments['label'] == 0].sample(len(positive_samples))
+    samples_train = positive_samples.append(negative_samples, ignore_index=True)
+    x_train = samples_train['sequence']
+    y_train = samples_train['label']
     processor = learn.preprocessing.ByteProcessor(SEGMENT_LENGTH)
     x = np.array(list(processor.fit_transform(x)))
-    x_train, x_validation, y_train, y_validation = cross_validation.train_test_split(x, y, test_size=0.2)
+    x_train = np.array(list(processor.fit_transform(x_train)))
     n_steps = len(x_train) // batch_size
-    validation_monitor = learn.monitors.ValidationMonitor(x_validation, y_validation, every_n_steps=n_steps,
-                                                          early_stopping_rounds=1)
+    # x_train, x_validation, y_train, y_validation = cross_validation.train_test_split(x, y, test_size=0.2)
+    # validation_monitor = learn.monitors.ValidationMonitor(x_validation, y_validation, every_n_steps=n_steps,
+    #                                                       early_stopping_rounds=1)
     classifier = learn.Estimator(model_fn=char_cnn_model)
     # classifier.fit(x_train, y_train, steps=math.inf, batch_size=batch_size, monitors=[validation_monitor])
     print(datetime.datetime.now() - begin)
-    classifier.fit(x_train, y_train, steps=n_steps, batch_size=batch_size)
+    classifier.fit(x_train, y_train, steps=1 * n_steps, batch_size=batch_size)
     print(datetime.datetime.now() - begin)
     y_predicted = [p['class'] for p in classifier.predict(x, batch_size=batch_size, as_iterable=True)]
-    print(datetime.datetime.now() - begin)
+    y_train_predicted = [p['class'] for p in classifier.predict(x_train, batch_size=batch_size, as_iterable=True)]
     # print('Accuracy: {0:f}'.format(metrics.accuracy_score(y, y_predicted)))
     print(metrics.confusion_matrix(y, y_predicted))
+    print(metrics.confusion_matrix(y_train, y_train_predicted))
+    print(datetime.datetime.now() - begin)
 
 
 if __name__ == '__main__':
